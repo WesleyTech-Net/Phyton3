73a4c887deb81f796dab3bd9fe1f508d72f621d1
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-01 11:20:07 -0300

    Create expressões.py

---
+++ expressões.py
@@ -0,0 +1,6 @@
+#calculando as paredes 
+qtdp = int(input('Quantas paredes: '))
+largurap = float(input('Insira a largura: '))
+alturap = float(input('Insira a altura: '))
+totalp = qtdp * (largurap * alturap)
+print(f'A área total de(as) {qtdp} parede(s) 7.5 é de {totalp}m²')
