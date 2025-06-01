6eda9cad7b4e5fd19628de867c49bd16c2b7e2ff
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-01 10:35:44 -0300

    Update math.py

--- math.py
+++ math.py
@@ -3,6 +3,6 @@ disciplina = str(input('Disciplina: '))
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 n3 = float(input('Digite a terceira nota: '))
-n4 = float(input('Digite a quarta nota: '))
+n4 = float(input('Digite a quarta nota: ')) 
 m = (n1 + n2 + n3 + n4) / 4
 print(f'O aluno {aluno} obteve uma média {m} pontos, na disciplina de {disciplina}')
