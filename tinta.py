030b3a42a0fda564352f1f30ba6a2b82b3a04f80
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-01 11:22:41 -0300

    Update tinta.py

--- tinta.py
+++ tinta.py
@@ -1,6 +1,11 @@
-#calculando as paredes 
+#calculando a área da parede 
 qtdp = int(input('Quantas paredes: '))
 largurap = float(input('Insira a largura: '))
 alturap = float(input('Insira a altura: '))
 totalp = qtdp * (largurap * alturap)
-print(f'A área total de(as) {qtdp} parede(s) 7.5 é de {totalp}m²')
+print(f'A área total de(as) {qtdp} parede(s) é de {totalp} m²')
+
+#calculando a quantidade de tinta necessária 
+rendimento = float(input('Qual o rendimento da tinta: '))
+qtdemaos = int(input('Insira a quantidade de demãos: '))
+
