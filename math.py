a0667d295b65497ea540b554fc3e80034573d96b
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-01 10:41:20 -0300

    Delete math.py

--- math.py
+++
@@ -1,8 +0,0 @@
-aluno = str(input('Nome do aluno: '))
-disciplina = str(input('Disciplina: '))
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
-n3 = float(input('Digite a terceira nota: '))
-n4 = float(input('Digite a quarta nota: ')) 
-m = (n1 + n2 + n3 + n4) / 4
-print(f'O aluno {aluno} obteve uma média {m} pontos, na disciplina de {disciplina}')
