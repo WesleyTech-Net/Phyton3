7da64a42f755b953d61a44aad72e498f5685fe02
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-02 21:54:42 -0300

    Create aulapratica1.py

---
+++ aulapratica1.py
@@ -0,0 +1,58 @@
+#QUESTÃO 1
+#letra A
+print(1 + 2+ 3 + 4 + 5)
+
+#letra B
+print((23 + 19 + 31) / 3)
+
+#letra c
+print(403 // 73)
+
+#letra d
+print(403 % 73)
+
+#letra e
+print(2 ** 10)
+
+#letra f
+print(54 - 57)
+print(abs(54 - 57))
+
+#letra g
+print(min(34, 29, 31))
+
+#ATRIBUIÇÕES
+#letra a
+a = 3 
+
+#letra b
+b = 4
+
+#letra c
+c = a * a + b * b
+print(c)
+
+#STRING
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+
+#letra a
+res = s1 + ' ' + s2 + ' ' + s3
+print(res)
+
+#letra b
+res = 10 * (s1 + ' ')  
+print(res)
+
+#letra c
+resp = (s1 + ' ') + 2 * (s2 + ' ') + 3 * (s3 + ' ') 
+print(resp)
+
+#letra d
+resp = 7 * (s1 + s2 + ' ')
+print(resp)
+
+#letra e
+resp = 5 * (2 *s2 + s3 + ' ')
+print(resp)
