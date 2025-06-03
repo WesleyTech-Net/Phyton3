e82bd4de7d4645a6dc55a47d0a92b96975e4de10
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-03 18:28:08 -0300

    Create exercício4.py

---
+++ exercício4.py
@@ -0,0 +1,7 @@
+n1 = float(input('Insira um número: '))
+n2 = float(input('Insira outro número: '))
+
+if n1 > n2:
+    print(f'O número {n1} é maior que {n2}.')
+else:
+    print(f'O número {n2} é maior que {n1}.')
