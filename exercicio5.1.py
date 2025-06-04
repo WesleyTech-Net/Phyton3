1268f51ffebb9c895d6576d12360958803a69b24
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-03 23:12:17 -0300

    Create exercicio5.1.py

---
+++ exercicio5.1.py
@@ -0,0 +1,8 @@
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+n3 = float(input('Terceira nota: '))
+
+if n1 >= 7 and n2 >= 7 and n3 >= 7:
+    print('Você está aprovado!')
+else:
+    print('Você está reprovado!')
