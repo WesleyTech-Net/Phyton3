9088da450c0e170278581cc54499f57d51827688
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-05 23:42:06 -0300

    Create escolha.py

---
+++ escolha.py
@@ -0,0 +1,7 @@
+print('- CIMA')
+print('- BAIXO')
+escolha = input('Escolha: ')
+if escolha == 'CIMA' or escolha == 'BAIXO':
+    print(f'Você escolheu o lado {escolha}')
+else:
+    print('DECIDA-SE!')
