b6225ba32c0420f38467893faf7f832cb489e824
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-05 23:46:02 -0300

    Update jogo2.py

--- jogo2.py
+++ jogo2.py
@@ -2,4 +2,4 @@ print(10 * '-', 'ESCOLHA O LADO', 10 * '-')
 print('1 - NORTE', '\n2 - SUL', '\n3 - LESTE', '\n4 - OESTE')
 escolha = int(input('Escoolha um lado de 1 a 4: '))
 if escolha == 1 or escolha == 2 or escolha == 3 or escolha == 4:
-    print(f'O lado escolhido foi{escolha} VOCÊ ESCAPOU!')
+    print(f'VOCÊ ESCAPOU!')
