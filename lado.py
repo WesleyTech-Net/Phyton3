0aae8b735f4481a0fc90b28e09ced8893a32b7c8
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-05 23:46:05 -0300

    Update lado.py

--- lado.py
+++ lado.py
@@ -2,6 +2,8 @@ print(10 * ' ', 'CLASSIFICAÇÃO DE TRIÂNGULO', 10 * ' ')
 v1 = float(input('Primeiro valor: '))
 v2 = float(input('Segundo valor: '))
 v3 = float(input('Terceiro valor: '))
+
+#preciso corrigir
 if v1 == 0 or v2 == 0 or v3 == 0:
     print('ESSE TRIÂNGULO NÃO EXISTE!')
 elif v1 + v2 < v3 and v1 + v3 < v2 and v3 + v2 < v1:
