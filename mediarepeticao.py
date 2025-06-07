183ebb3ee8bbe500f41025245b6f05ac297736cf
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-07 18:07:14 -0300

    Update mediarepeticao.py

--- mediarepeticao.py
+++ mediarepeticao.py
@@ -1,11 +1,11 @@
 soma = 0
 cont = 1
 while (cont <=4):
-    x = int(input(f'Insira a {cont}º nota: '))
+    x = float(input(f'Insira a {cont}º nota: '))
     soma += x
     cont += 1
     media = soma /4
 if (x >= 70):
-    print(f'Sua nota foi {media}. VOCÊ ESTÁ APROVADO')
+    print(f'Sua nota foi {media:.2f}. VOCÊ ESTÁ APROVADO!')
 else:
-    print(f'Sua nota foi {media}. VOCÊ ESTÁ REPROVADO')
+    print(f'Sua nota foi {media:.2f}. VOCÊ ESTÁ REPROVADO!')
