a0103085ca990e922eb3ee497ef1b4154ce0706c
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-07 09:10:14 -0300

    Update repeticao.py

--- repeticao.py
+++ repeticao.py
@@ -2,3 +2,13 @@ x = 1
 while (x <= 200):
     print(x)
     x = x + 1 #incrementador 
+
+#exemplo
+incial = int(input('Valor inicial: '))
+final = int(input('Valor final: '))
+
+x = incial
+while (x <= final):
+    if (x % 2 == 0):
+        print(x)
+    x = x + 1
