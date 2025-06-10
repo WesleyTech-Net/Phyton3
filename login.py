2c317d6b58544e87f260ae9e4fda5617c34ba9f9
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-09 22:04:04 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,11 @@
+while True:
+    usuario = input('Usuário: ')
+    if usuario != 'wesleysamp.ws@gmail.com':
+        print('Usuário incorreto tento outra vez...')
+        continue #volta para o início do laço
+   
+    senha = input('Senha: ')
+    if (senha == 'Flute159357@'):
+        break #encerra o laço wes
+
+print('Acesso concedido!')
