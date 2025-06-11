760b01299fcaacf7c1fa047c6c0da87d3f916173
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-11 20:47:54 -0300

    Create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+tabuada  = 1
+while tabuada <= 10:
+    print(f'\nTABUADA DO {tabuada}')
+    i = 1
+    while i <= 10:
+        print(f'{tabuada} X {i} = {tabuada * i}')
+        i += 1
+    tabuada += 1
+#2 for
+for tabuada in range(1, 11, 1):
+    print(f'\nTABUADA DO {tabuada}')
+    i = 1
+    for i in range (1, 11, 1):
+        print(f'{tabuada} * {i}: {tabuada * 1}')
+
