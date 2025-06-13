a422bd1fbbcf0bc7ebb35647a39f8ce84ca5d2fc
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-13 20:33:26 -0300

    Create boolean.py

---
+++ course_pyton_linkedin/boolean.py
@@ -0,0 +1,18 @@
+light_is_on = True
+
+if light_is_on:
+    print("The light is on!")
+    print("Hello")
+
+# Find something around you that could be represented by a Boolean and make a variable for it 
+
+hungry = True
+
+if hungry:
+    print("I'm hungry")
+    print('Bring me food')
+
+computer_is_off = True
+
+if computer_is_off:
+    print('Your computer is off')
