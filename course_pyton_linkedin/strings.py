4e4c51ec26d72af83b4d8b726ee1b83ea1d387fa
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-13 20:15:08 -0300

    Create strings.py

---
+++ course_pyton_linkedin/strings.py
@@ -0,0 +1,19 @@
+day = 14
+
+month = 'June'
+
+temp = 9
+
+print(f"Today is {month} {day} and it's {temp} degrees outside")
+
+#Make a variable called day_name and set it to day of the weeke like Tuesday and add it to the string we print
+
+day_name = 'Friday'
+
+month = 'June'
+
+day = 13
+
+temp = -2
+
+print(f"Today is {day_name} on {month} {day} and it's {temp} degrees outside")
