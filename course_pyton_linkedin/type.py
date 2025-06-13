a64de5206a5fc89d6769226024b523cdf1e9fe7b
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-13 20:04:08 -0300

    Create type.py

---
+++ course_pyton_linkedin/type.py
@@ -0,0 +1,36 @@
+day = 21
+
+temp = -15
+
+weight = 190.4565658
+
+print(day + 3)
+
+print(weight * 2)
+
+print(weight / 2)
+
+print(weight - 2)
+
+height = 1.69
+
+print(height)
+
+age = 32
+
+print(age)
+
+shirt = 'blue'
+
+print(shirt)
+
+store = 'Nick\'s Pizza Shop, the "best" there is'
+print(store)
+
+#Put the name of a movie you like into a variable called movie
+
+movie = 'Interestellar'
+print(movie)
+
+series = 'How i met your "mother"'
+print(series)
