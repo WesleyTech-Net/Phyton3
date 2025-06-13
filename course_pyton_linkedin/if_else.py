cb3a739b9b835529c999980210eb63c65016a844
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-13 20:59:18 -0300

    Update if_else.py

--- course_pyton_linkedin/if_else.py
+++ course_pyton_linkedin/if_else.py
@@ -4,3 +4,19 @@ if study:
     print("I need study to the exam")
 else:
     print("I'm prepare")
+
+weight = 77
+
+if weight < 80:
+    print("You're under weight")
+else:
+    print("You're overweight")
+
+#Make a variable age, and if someone is 18 years or older, print that they are an adult. Otherwise print that they are a child
+
+age = 17
+
+if age >= 18:
+    print("You're an adult!")
+else:
+    print("You're a child!")
