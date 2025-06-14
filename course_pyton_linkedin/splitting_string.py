c500e69d6db3304c8b2c4483c0108689dcf6dd62
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-14 09:40:37 -0300

    New

---
+++ course_pyton_linkedin/splitting_string.py
@@ -0,0 +1,10 @@
+text = """Named for John G. Nicolay, President Lincoln's personal secretary, this is considered the "first draft" of the speech, begun in Washington on White house stationery. 
+The second page is writen on different paper stock, indicating it was finished in Gettysburg before the cemetery dedication began. Lincoln gave this draft to Nicolay, 
+who went to Gettysburg with Lincoln and witnessed the speech. The Library of Congress owns this manuscript.
+
+Four score and seven years ago our fathers brought forth, upon this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.
+"""
+
+print(text.split())
+
+print(len(text.split()))
