418b6df144617b753ba3efc29aace745e3a741cf
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-13 23:31:28 -0300

    Create dic.py

---
+++ course_pyton_linkedin/dic.py
@@ -0,0 +1,28 @@
+cats = {"Mel":4, "Tom":9, "Jerry":12}
+
+print(cats)
+
+#add 
+
+cats = {"Mel":4, "Tom":9, "Jerry":12}
+
+cats ["Loq"] = 1
+
+print(cats)
+
+#delete
+cats = {"Mel":4, "Tom":9, "Jerry":12}
+
+del(cats["Mel"])
+
+print(cats)
+
+#lenght
+
+cats = {"Mel":4, "Tom":9, "Jerry":12}
+
+len(cats)
+
+print(len(cats))
+
+
