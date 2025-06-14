bb93e08f93426416ede44db309237ed61e2b7e21
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-14 11:42:48 -0300

    new

--- exercicio6.py
+++ exercicio6.py
@@ -1,6 +1,28 @@
+#inteiros de 0 a 9 com passo de 2, excluindo o 9
 soma = 0 
 qtd = 0
 for i in range(0, 9, 2):
+     print(i,'\n')
+     soma += 1
+     qtd += 1
+
+i = 0
+
+while i < 9:
+     print(i)
+     i += 2
+
+# Inteiros de 3 até 12, com 12 incluso
+
+som = 0
+qtd = 0
+
+for i in range(3, 13):
      print(i)
      soma += 1
-     qtd += 1       
+     qtd += 1
+
+i = 3
+while (i < 13):
+     print(i)
+     i += 1
