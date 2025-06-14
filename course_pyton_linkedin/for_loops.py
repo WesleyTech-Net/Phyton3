81250416f61a0ce0a912210b247594c9e894a11d
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-13 23:11:39 -0300

    Update for_loops.py

---
+++ course_pyton_linkedin/for_loops.py
@@ -0,0 +1,12 @@
+for number in range (10):
+    print(number)
+
+fav_food = ['Pizza', 'Lasanha', 'Sushi', 'Hamburguer', 'Strogonoff']
+
+for food in fav_food:
+    print(food, end=' \n')
+
+#Loop 40 times and print the number of loop times 2.
+
+for number in range(40):
+    print((number + 1) * 2)
