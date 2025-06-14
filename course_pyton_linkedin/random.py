afda613347702e7ed184a527efd98f7193ec095d
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-13 21:11:34 -0300

    Create random.py

---
+++ course_pyton_linkedin/random.py
@@ -0,0 +1,10 @@
+def your_code():
+    
+    number = 7
+
+    if number % 2 == 0:
+      print(f'The number {number} is even')
+    else:
+      print(f'The number {number} is odd.')
+
+    return False
