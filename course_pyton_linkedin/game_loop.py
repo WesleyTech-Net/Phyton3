f7496a08aba33cf55ff455a2638533ff807b31d8
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-15 10:59:41 -0300

    Update game_loop.py

--- course_pyton_linkedin/game_loop.py
+++ course_pyton_linkedin/game_loop.py
@@ -1,11 +1,22 @@
 import random
+import time 
+
+print("\nWelcome to the GUESSING GAME! I'm goind to pick up a number between 1 and 100.\n")
+
+time.sleep(2)
+print("Thinking a number...")
+time.sleep(2)
 
 guess = int(input("Whats is your guess? "))
-correct_number = random.randint(1, 5)
+correct_number = random.randint(1, 100)
 guess_count = 1
 
 while guess != correct_number:
     guess_count += 1
-    guess = int(input("Whats is your guess? "))
+    if guess < correct_number:
+        guess = int(input("Wrong! You need to guess higher. Whats is your guess? "))
+    else:
+         guess = int(input("Wrong! You need to guess lower. Whats is your guess? "))
+
 
 print(f"Congrats! The right answer was {correct_number}. It took you {guess_count} guesses.")
