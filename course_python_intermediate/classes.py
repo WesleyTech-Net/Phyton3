013940cf6315676edcca8eb398c7220f4c01dbc5
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-15 13:49:05 -0300

    new

---
+++ course_python_intermediate/classes.py
@@ -0,0 +1,12 @@
+class School:
+    info = "My English class is on Tuesday"
+
+print(School.info)
+
+#Create a class for something around you in the room.
+#Create a class variable inside the class
+
+class Room:
+    my_room = ["Moniotor", "Chair", "Bed", "Desk"]
+    for my_room in my_room:
+        print(my_room, end=' ')
