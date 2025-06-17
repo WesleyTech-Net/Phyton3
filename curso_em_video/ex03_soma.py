709f428a6a6655962ea212d232549ded0604a698
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-17 20:12:04 -0300

    new

---
+++ curso_em_video/ex03_soma.py
@@ -0,0 +1,4 @@
+num1 = int(input("Primeiro número: "))
+num2 = int(input("Segundo número: "))
+soma = num1 + num2
+print(f"A soma entre o número {num1} e o {num2} é igual a {soma}")
---
+++ curso_em_video/ex04.py
@@ -0,0 +1,3 @@
+a = input("Digite algo: ")
+print(f"O tipo primitivo desse valor é {type(a)}")
+print(f"Só tem espacços? {a.isspace()}")
