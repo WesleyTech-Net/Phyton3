7accd3c04151dbf501afbc8488118cfb4610c269
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-17 17:37:02 -0300

    NEW

---
+++ course_pyton_linkedin_begginer/funcao+parametro.py
@@ -0,0 +1,8 @@
+def realce(s1):
+    print("|", 20 * "_", "|")
+    print("|", 20 * "_", "|")
+    print(s1)
+    print("|", 20 * "_", "|")
+    print("|", 20 * "_", "|")
+
+realce("BEM VINDO AO PROGRAMA")
