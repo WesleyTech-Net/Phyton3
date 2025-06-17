d3e192581594a0ac41c67de9f1c8d4dd608f194f
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-17 20:25:35 -0300

    new

---
+++ curso_em_video/ex06.py
@@ -0,0 +1,11 @@
+import math
+
+num = int(input("Digite um valor: "))
+dobro = num * 2
+triplo = num * 3
+rq = math.sqrt(num)
+
+print(f"O dobro de {num} é {dobro}")
+print(f"O triplo de {num} é {triplo}")
+print(f"A raiz quadrada de {num} é {rq:.2f}")
+
---
+++ curso_em_video/ex07_media_nota.py
@@ -0,0 +1,7 @@
+n1 = float(input("Primeira nota: "))
+n2 = float(input("Segunda nota: "))
+n3 = float(input("Terceira nota: "))
+n4 = float(input("Quarta nota: "))
+media = (n1 + n2 + n3 + n4) / 4
+
+print(f"A média final é de {media} pontos")
