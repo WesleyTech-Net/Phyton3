6e6c4f4af1648ef7ff3ca1423e348010897c908a
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-21 12:53:01 -0300

    new

--- curso_em_video/ex15_aluguel_carro.py
+++ curso_em_video/ex15_aluguel_carro.py
@@ -3,4 +3,4 @@ distancia = float(input("Insira os km rodados: "))
 diaria = dias * 60
 kmRodado = distancia * 0.15
 valorTotal = diaria + kmRodado
-print(f"O total a pagar é de R${valorTotal}")
+print(f"O total a pagar é de R${valorTotal:.2f}")
---
+++ curso_em_video/ex16_quebrando_numero.py
@@ -0,0 +1,3 @@
+import math
+numero = float(input("Insira um valor: "))
+print(f"O valor digitado foi {numero} e a sua porção inteira é {math.trunc(numero)}")
