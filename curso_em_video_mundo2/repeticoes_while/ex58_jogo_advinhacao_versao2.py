29a9fff225e243607ba2888bc0c99300309acef0
WesleyTech-Net <wesley.tech.samp@gmail.com>
2025-06-26 12:09:51 -0300

    new

--- curso_em_video_mundo2/repeticoes_while/ex58_jogo_advinhacao_versao2.py
+++ curso_em_video_mundo2/repeticoes_while/ex58_jogo_advinhacao_versao2.py
@@ -11,15 +11,18 @@ cont = 0
 
 while True:
     try:
+
         jogador = int(input("Em que número eu pensei (0 até 5)? "))
         computador = randint(0, 5)
         print("Processando...")
         sleep(2)
+
         if jogador == computador:
             cont += 1
             print(f"Eu pensei no número {computador}. PARABÉNS VOCÊ ACERTOU!")
             print(f"No total foram {cont} jogadas.")
             escolha = str(input("Deseja jogar novamente (S/N): ")).strip()
+            
             if escolha in "Ss":
                 continue
             elif escolha in "Nn":
@@ -29,6 +32,7 @@ while True:
             cont += 1
             print(f"Eu pensei no número {computador}. Você ERROU! Tente novamente...")
             continue
+
     except ValueError:
         print("Opção inválida. Tente novamente...")
         continue
