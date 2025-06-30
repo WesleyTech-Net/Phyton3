c9a7bc68e46cc9018dd32700a2594434ce5bbe79
Wesley <wesley.tech.samp@gmail.com>
2025-06-30 11:56:03 -0300

    correction

--- curso_em_video_mundo2/repeticoes_while/ex64_tratando_varios_valores_versao_1.py
+++ curso_em_video_mundo2/repeticoes_while/ex64_tratando_varios_valores_versao_1.py
@@ -2,7 +2,9 @@ value = int(input("Digite um número (999 para parar): "))
 
 soma = 0
 cont = 0
+
 while value != 999:
+
     cont += 1
     soma += value
     value = int(input("Digite um número (999 para parar): "))
