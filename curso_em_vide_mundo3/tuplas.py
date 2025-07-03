db50b774afb6e0d21dddc221c42a1b933fd1f9cd
Wesley <wesley.tech.samp@gmail.com>
2025-07-03 19:05:06 -0300

    new

---
+++ curso_em_vide_mundo3/tuplas.py
@@ -0,0 +1,11 @@
+#tupla utiliza parênteses ou sem parênteses
+lanche = 'Hamburguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche[0])
+print(lanche[1])
+print(lanche[2])
+print(lanche[3])
+print(lanche[1:3])
+print(lanche[2:])
+print(lanche[:3])
+print(lanche[-1])
+print(lanche[-3:])
