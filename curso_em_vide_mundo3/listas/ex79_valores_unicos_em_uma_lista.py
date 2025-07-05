6a82bd79ce99bc303c7b0053ba024a4145808fce
Wesley <wesley.tech.samp@gmail.com>
2025-07-05 15:41:38 -0300

    create ex79

---
+++ curso_em_vide_mundo3/listas/ex79_valores_unicos_em_uma_lista.py
@@ -0,0 +1,31 @@
+valores = list()
+
+cont = 1
+while True:
+
+    try:
+        
+        num = (int(input(f"Digite o {cont}º valor: ")))
+        
+        
+        
+        if num in valores:
+            print("Valor já inserido tente novamente...")
+
+        else:
+            cont += 1
+            valores.append(num)
+            print("✅ Valor adicionado com sucesso...")
+            op = str(input("Quer continuar: [S/N] ")).strip().upper()[0]
+            if op in "S":
+                continue
+            elif op in "N":
+                break
+        
+    except ValueError:
+        print("Valor inválido tente novamente...")
+        continue
+
+print("Você adicionou os valores: ", end="")
+for i in valores:
+    print(f"{i}", end=" ")
