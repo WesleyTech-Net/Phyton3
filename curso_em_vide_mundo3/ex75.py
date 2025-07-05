d40bc625815f127350f2fe31eec145b4d4232264
Wesley <wesley.tech.samp@gmail.com>
2025-07-05 09:50:45 -0300

    update

--- curso_em_vide_mundo3/ex75.py
+++ curso_em_vide_mundo3/ex75.py
@@ -3,3 +3,15 @@ tupla = (int(input("Digite um número: ")), int(input("Digite um número: ")), i
 print(f"Você inseriu os valores: ", end="")
 for i in tupla:
     print(f'{i}', end=" ")
+
+print("\nOs valores pares digitados são: ", end=" ")
+for pares in tupla:
+    if pares % 2 == 0:
+        print(pares, end=" ")
+
+print(f"\nO valor 5 apareceu {tupla.count(5)} vezes.")
+
+if 2 in tupla:
+    print(f"O valor 2 está na posição {tupla.index(2) + 1}")
+else:
+    print("O valor 2 não foi encontrado em nunhuma posição!")
