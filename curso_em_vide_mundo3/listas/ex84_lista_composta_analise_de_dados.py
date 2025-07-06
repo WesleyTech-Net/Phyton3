707640107611f803a5e1bbcb1cbf4634af12aa48
Wesley <wesley.tech.samp@gmail.com>
2025-07-06 09:53:14 -0300

    correction

--- curso_em_vide_mundo3/listas/ex84_lista_composta_analise_de_dados.py
+++ curso_em_vide_mundo3/listas/ex84_lista_composta_analise_de_dados.py
@@ -2,6 +2,7 @@ pessoas = list()
 dado = list()
 cont = 1
 cont2 = 0
+maior_peso = menor_peso = 0
 while True:
 
     print(f"{cont}º pessoa")
@@ -10,6 +11,15 @@ while True:
 
         dado.append(str(input("Nome: ").strip()))
         dado.append(float(input("Peso: ")))
+
+        if len(pessoas) == 0:
+            maior_peso = menor_peso = dado[1]
+        else:
+            if dado[1] > maior_peso:
+                maior_peso = dado[1]
+            if dado[1] < menor_peso:
+                menor_peso = dado [1]
+
         print("Dado cadastrado com sucesso...")
         pessoas.append(dado[:])
         dado.clear()
@@ -25,7 +35,16 @@ while True:
         print("❌ Valor inválido tente novamente...")
 
 print(f"Total de pessoas cadastradas: {cont2}")
+print(f"O maior peso foi de {maior_peso} kg.", end=" ")
 
+for i in pessoas:
+    if i[1] == maior_peso:
+        print(f"Peso de {i[0]}")
+
+print(f"O menor peso foi de {menor_peso} kg")
+for i in pessoas:
+    if i[1] == menor_peso:
+        print(f"Peso de {i[0]}")
 
 
 
