ab4cca4d773e8bec564fd2524fa9a0f59ff3edd5
Wesley <wesley.tech.samp@gmail.com>
2025-07-08 11:41:29 -0300

    new program

---
+++ ALURA/ex2_coleta_e_amostragem_de_dados.py
@@ -0,0 +1,4 @@
+nome = str(input("Digite seu nome: "))
+idade = int(input("Sua idade: "))
+altura = float(input("Sua altura: "))
+print(f"Olá, {nome}, você tem {idade} anos de idade e tem {altura} metros.")
