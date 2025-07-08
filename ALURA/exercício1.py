51dcb8264e361c2607cae73689637c99b3d2c57f
Wesley <wesley.tech.samp@gmail.com>
2025-07-08 07:26:08 -0300

    new

--- ALURA/exercício1.py
+++ ALURA/exercício1.py
@@ -15,3 +15,6 @@ print("Y")
 
 #Imprima o dia do seu nascimento em formato dia mês ano. Lembrando que os valores de dia e ano não podem estar entre aspas. 
 print(13, "maio de", 1993)
+
+#Imprima, em um único print, o atual ano que você está fazendo esse curso. 
+print("Ano atual: 2025  ")
