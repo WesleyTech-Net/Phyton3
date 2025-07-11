0e553312c82184b69b291a278b35853cbae05965
Wesley <wesley.tech.samp@gmail.com>
2025-07-11 09:08:47 -0300

    update

--- ALURA/estruturas de repetição/estruturas_de_repeticao.py
+++ ALURA/estruturas de repetição/estruturas_de_repeticao.py
@@ -1,6 +1,20 @@
-#1) Escreva um programa que peça dois números inteiros e imprima todos os números inteiros entre eles.
-numero1 = int(input("Primeiro valor: "))
-numero2 = int(input("Segundo valor: "))
 
-for i in range(numero1, numero2 + 1):
-    print(f"{i}")
+
+
+#4) Desenvolva um programa que leia um conjunto indeterminado de temperaturas em Celsius e informe a média delas. 
+# A leitura deve ser encerrada ao ser enviado o valor -273°C.
+
+temperaturas = int(input("Insira a temperatura (para sair digite -273): "))
+
+soma = 0
+cont = 0
+while temperaturas != 273:
+
+    temperaturas = int(input("Insira a temperatura (para sair digite -273): "))
+
+    cont += 1
+    soma += temperaturas
+    media = soma / cont
+
+print(f"Você adicionou {cont} temperaturas. A média entre elas é igual a {media:.2f}.")
+
