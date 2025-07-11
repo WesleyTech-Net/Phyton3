cafb4ff625d9b1f0933bc6bd5b34733fc530abc0
Wesley <wesley.tech.samp@gmail.com>
2025-07-11 19:19:06 -0300

    create pacotes.py

---
+++ ALURA/bibliotecas/pacotes.py
@@ -0,0 +1,12 @@
+
+
+#importando uma função específica de uma biblioteca
+estudante_2 = ["João", "Maria", "Pedro", "José", "Vitor"]
+
+from random import choice
+
+estudante_escolha = choice(estudante_2)
+
+print(estudante_escolha)
+
+
