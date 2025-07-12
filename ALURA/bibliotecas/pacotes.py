96a6b817f84156b5cc61341378ed8b1003886ad0
Wesley <wesley.tech.samp@gmail.com>
2025-07-11 22:53:15 -0300

    update pacotes

--- ALURA/bibliotecas/pacotes.py
+++ ALURA/bibliotecas/pacotes.py
@@ -1,4 +1,11 @@
+#utilizando BAR
+import matplotlib.pyplot as plt
 
+estudante = ["João", "Maria", "Pedro"]
+notas = [8, 9, 6.5]
+
+plt.bar(x = estudante, height = notas)
+plt.show()
 
 #importando uma função específica de uma biblioteca
 estudante_2 = ["João", "Maria", "Pedro", "José", "Vitor"]
