574ab2218ab77b913b613ea343ce2fa353be759a
Wesley <wesley.tech.samp@gmail.com>
2025-07-18 09:41:02 -0300

    update

---
+++ ALURA/funções/projeto01.py
@@ -0,0 +1,8 @@
+lista = [16, 14, 63, 65, 17, 99, 70, 11, 20, 48, 79, 32, 17, 89, 12, 25, 66]
+
+tamanho_listaa = len(lista)
+maior_valor = max(lista)
+menor_valor = min(lista)
+total = sum(lista)
+
+print(f"O tamanho da lista é {tamanho_listaa}.\nO maior valor é {maior_valor}.\nO menor valor é {menor_valor}.\nA soma de todos itens da lista é igual a {total}")
